0cf2ccb22b3930bd715396e049cff30789d1ecde
siddharth77 <siddhartha@Siddharthas-MBP.home>
2017-04-04 23:08:57 -0400

    final

--- server.js
+++ server.js
@@ -18,6 +18,6 @@ app.use (function(req, res, next) {
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on + PORT');
+  console.log('Express server is up on' + PORT);
 });
 
